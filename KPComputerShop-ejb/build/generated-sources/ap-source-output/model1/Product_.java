0749d5e28cd91ec645b7c0c7e5abed33713627e9
Sega <alexeevsergey888@gmail.com>
2016-09-18 23:23:04 +0300

    last commit

--- KPComputerShop-ejb/build/generated-sources/ap-source-output/model1/Product_.java
+++ KPComputerShop-ejb/build/generated-sources/ap-source-output/model1/Product_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-09-11T13:48:45")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-09-14T23:01:29")
 @StaticMetamodel(Product.class)
 public class Product_ { 
 
