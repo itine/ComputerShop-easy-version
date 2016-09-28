e47add0bc0983bfdcab8eabe7f03568ef378ac2f
Sega <alexeevsergey888@gmail.com>
2016-09-29 00:28:43 +0300

    created errors pages and page accesses.

--- KPComputerShop-ejb/build/generated-sources/ap-source-output/model1/UserRole_.java
+++ KPComputerShop-ejb/build/generated-sources/ap-source-output/model1/UserRole_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-09-14T23:01:29")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-09-22T18:44:12")
 @StaticMetamodel(UserRole.class)
 public class UserRole_ { 
 
--- KPComputerShop-ejb/build/generated-sources/ap-source-output/model1/Users_.java
+++ KPComputerShop-ejb/build/generated-sources/ap-source-output/model1/Users_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import model1.Orders;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-09-14T23:01:29")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-09-22T18:44:12")
 @StaticMetamodel(Users.class)
 public class Users_ { 
 
